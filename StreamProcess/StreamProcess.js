a6522b033c5e499f577fc2e1335f34710cd6af58
JoaoVitorML-BR <jvmlira33@gmail.com>
2023-03-06 15:16:24 -0300

    add stream-process video

---
+++ StreamProcess/StreamProcess.js
@@ -0,0 +1,35 @@
+const { createReadStream } = require("fs");
+const { spawn } = require("child_process");
+
+exports.ProcessingVideo = (req, res) => {
+  const videoPath = "./videos/howuse.mp4";
+  const ffmpegProcess = spawn("ffmpeg", [
+    "-i", "pipe:0",
+    "-f", "mp4",
+    "-vcodec", "h264",
+    "-acodec", "aac",
+    "-movflags", "frag_keyframe+empty_moov+default_base_moof",
+    "-b:v", "1500k",
+    "-maxrate", "1500k",
+    "-bufsize", "1000k",
+    "-f", "mp4",
+    "-vf", "monochrome,drawtext=text='':x=10:y=H-th-10:fontsize=50:fontcolor=yellow:shadowcolor=black:shadowx=5:shadowy=5",
+    "pipe:1"
+  ], {
+    stdio: ["pipe", "pipe", "pipe"]
+  });
+
+  // Iniciar o processo de stream do vídeo
+  createReadStream(videoPath).pipe(ffmpegProcess.stdin);
+
+  // Enviar os dados de saída do processo do FFMPEG para o cliente
+  ffmpegProcess.stdout.pipe(res);
+
+  // Gerenciar a finalização do processo de streaming
+  req.on("close", () => {
+    ffmpegProcess.stdout.destroy();
+    ffmpegProcess.stdin.destroy();
+    console.log("Client disconnected, killing FFMPEG process...");
+    ffmpegProcess.kill();
+  });
+};
--- routes/routes.js
+++ routes/routes.js
@@ -11,61 +11,65 @@ const SSDController = require('../controllers/SSDController');
 const GPUController = require('../controllers/GPUController');
 const FonteController = require("../controllers/FonteController");
 
+const ProcessVideo = require("../StreamProcess/StreamProcess");
+
+router.get('/video-process', ProcessVideo.ProcessingVideo); 
+
 // CRUD AMD_MOTHEBOARDS
 router.get('/motheboard/AMD', AmdMotheboardController.index);
 router.post('/motheboard/AMD', AmdMotheboardController.create);
 router.get('/motheboard/:id', AmdMotheboardController.findMotheboard);
 router.put('/motheboard', AmdMotheboardController.edit);
-router.delete('/motheboard/:id', AmdMotheboardController.remove); 
+// router.delete('/motheboard/:id', AmdMotheboardController.remove); 
 
 // CRUD MOTHEBOARDS-INTEL
 router.get('/motheboardintel/INTEL', IntelMotheboardsController.index);
 router.post('/motheboard/INTEL', IntelMotheboardsController.createIntelMotheboard);
 router.get('/motheboard/INTEL/:id', IntelMotheboardsController.findMotheboardIntel);
 router.put('/motheboard/INTEL', IntelMotheboardsController.edit);
-router.delete('/motheboard/INTEL/:id', IntelMotheboardsController.remove);
+// router.delete('/motheboard/INTEL/:id', IntelMotheboardsController.remove);
 
 // CRUD CPUS-AMD
 router.get('/cpu/AMD',  AmdCpusController.index);
 router.get('/cpu/AMD/:id',  AmdCpusController.findCpu);
 router.post('/cpu/AMD', AmdCpusController.create);
 router.put('/cpu/AMD', AmdCpusController.edit);
-router.delete('/cpu/AMD/:id', AmdCpusController.remove);
+// router.delete('/cpu/AMD/:id', AmdCpusController.remove);
 
 // CRUD CPUS-INTEL
 router.get('/cpu/INTEL',  IntelCpusController.index);
 router.get('/cpu/INTEL/:id',  IntelCpusController.findCpu);
 router.post('/cpu/INTEL', IntelCpusController.create);
 router.put('/cpu/INTEL', IntelCpusController.edit);
-router.delete('/cpu/INTEL/:id', IntelCpusController.remove);
+// router.delete('/cpu/INTEL/:id', IntelCpusController.remove);
 
 // CRUD MEMORY-RAM-ALL
 router.get('/memoryram', MemoryRamController.index);
 router.get('/memoryram/:id', MemoryRamController.findMemoryRam);
 router.post('/memoryram', MemoryRamController.create);
 router.put('/memoryram', MemoryRamController.edit);
-router.delete('/memoryram/:id', MemoryRamController.remove);
+// router.delete('/memoryram/:id', MemoryRamController.remove);
 
 // CRUD SSD-ALL
 router.get('/ssd', SSDController.index);
 router.get('/ssd/:id', SSDController.findSSD);
 router.post('/ssd', SSDController.create);
 router.put('/ssd', SSDController.edit);
-router.delete('/ssd/:id', SSDController.remove);
+// router.delete('/ssd/:id', SSDController.remove);
 
 // CRUD GPU-ALL
 router.get('/gpu', GPUController.index);
 router.get('/gpu/:id', GPUController.findGPU);
 router.post('/gpu', GPUController.create);
 router.put('/gpu', GPUController.edit);
-router.delete('/gpu/:id', GPUController.remove);
+// router.delete('/gpu/:id', GPUController.remove);
 
 // CRUD FONTE-ALL
 router.get('/fonte', FonteController.index);
 router.get('/fonte/:id', FonteController.findFonte);
 router.post('/fonte', FonteController.create);
 router.put('/fonte', FonteController.edit);
-router.delete('/fonte/:id', FonteController.remove);
+// router.delete('/fonte/:id', FonteController.remove);
 
 // SEARCHING CPUS COMPATIBLES WITH MOTHEBOARD_ID / NAME
 router.get('/cpuscompatibles/AMD/:id', AmdMotheboardController.compatiblescpu);
